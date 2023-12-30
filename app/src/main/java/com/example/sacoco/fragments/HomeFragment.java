3a2966dc9b5ca1649999f2891daf38d1c2e3f6b8
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2023-12-30 14:32:22 +0100

    Update HomeFragment.java
    Add necessary logic into fragment

--- app/src/main/java/com/example/sacoco/fragments/HomeFragment.java
+++ app/src/main/java/com/example/sacoco/fragments/HomeFragment.java
@@ -1,11 +1,66 @@
 package com.example.sacoco.fragments;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sacoco.MainActivity;
 import com.example.sacoco.R;
+import com.example.sacoco.adapter.BagAdapter;
+import com.example.sacoco.cominterface.CardAction;
+import com.example.sacoco.viewmodels.BagViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class HomeFragment extends Fragment {
+import java.util.Objects;
+
+public class HomeFragment extends Fragment implements CardAction {
+    private final BagViewModel bagViewModel;
+    private RecyclerView bagsRecyclerView;
+
     public HomeFragment() {
         super(R.layout.fragment_base_layout);
+        bagViewModel = new ViewModelProvider(requireActivity()).get(BagViewModel.class);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        bagsRecyclerView = view.findViewById(R.id.contentListView);
+        bagsRecyclerView.setAdapter(new BagAdapter(bagViewModel.getBagsLiveData().getValue(), this));
+
+        bagViewModel.getBagsLiveData().observe(this.getViewLifecycleOwner(),
+                bagArrayList -> Objects.requireNonNull(bagsRecyclerView.getAdapter()).notifyItemInserted(bagArrayList.size()));
+
+        FloatingActionButton addBagButton = view.findViewById(R.id.addContentButton);
+        addBagButton.setOnClickListener(addBagButtonClickedListener);
+    }
+
+    private final View.OnClickListener addBagButtonClickedListener = view -> {
+        //TODO open dialog fragment here
+    };
+
+    @Override
+    public void onCardConsultButtonClicked(int bagSelectedIndex) {
+        bagViewModel.setSelectedBagLiveData(
+                Objects.requireNonNull(bagViewModel.getBagsLiveData().getValue()).get(bagSelectedIndex).getStartDate(),
+                bagViewModel.getBagsLiveData().getValue().get(bagSelectedIndex).getEndDate()
+        );
+
+        MainActivity mainActivityInstance = (MainActivity) requireActivity();
+        mainActivityInstance.loadFragment(BagDetailsFragment.class);
+    }
+
+    @Override
+    public void onCardRemoveButtonClicked(int bagSelectedIndex) {
+        bagViewModel.removeBag(
+                Objects.requireNonNull(bagViewModel.getBagsLiveData().getValue()).get(bagSelectedIndex).getStartDate(),
+                bagViewModel.getBagsLiveData().getValue().get(bagSelectedIndex).getEndDate()
+        );
     }
 }
