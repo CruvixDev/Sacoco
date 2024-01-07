78e70bd80ee4b8b11ec9b1db79c378f3b4dcb319
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2024-01-07 15:27:09 +0100

    Inflate AddBagDialogFragment in onCreateView
    Inflate AddBagDialogFragment in onCreateView methodes instead of constructor

--- app/src/main/java/com/example/sacoco/dialogs/AddBagDialogFragment.java
+++ app/src/main/java/com/example/sacoco/dialogs/AddBagDialogFragment.java
@@ -2,16 +2,23 @@ package com.example.sacoco.dialogs;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.sacoco.R;
 
 public class AddBagDialogFragment extends DialogFragment {
-    public AddBagDialogFragment() {
-        super(R.layout.dialog_add_bag_layout);
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_add_bag_layout, container, false);
     }
 
     @NonNull
