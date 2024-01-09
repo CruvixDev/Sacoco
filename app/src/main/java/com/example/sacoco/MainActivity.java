08e0bb7baf30531ed505676c61a7261ff9e6b53f
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2024-01-09 21:03:02 +0100

    Remove text title into main activity layout
    Remove text title text view into main activity and use app title instead

--- app/src/main/java/com/example/sacoco/MainActivity.java
+++ app/src/main/java/com/example/sacoco/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.example.sacoco.fragments.ClothesListFragment;
 import com.example.sacoco.fragments.EmailFragment;
@@ -16,16 +15,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView activityTitle;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_layout);
 
         String activityBaseTitle = getString(R.string.main_activity_title_base);
-        activityTitle = findViewById(R.id.mainActivityBaseTitle);
-        activityTitle.setText(String.format(activityBaseTitle, getString(R.string.main_activity_menu_home)));
+        this.setTitle(String.format(activityBaseTitle, getString(R.string.main_activity_menu_home)));
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnItemSelectedListener(onItemSelectedListener);
@@ -37,19 +33,19 @@ public class MainActivity extends AppCompatActivity {
 
         if (itemId == R.id.home) {
             loadFragment(HomeFragment.class);
-            activityTitle.setText(String.format(activityBaseTitle, getString(R.string.main_activity_menu_home)));
+            this.setTitle(String.format(activityBaseTitle, getString(R.string.main_activity_menu_home)));
         }
         else if (itemId == R.id.clothes_list) {
             loadFragment(ClothesListFragment.class);
-            activityTitle.setText(String.format(activityBaseTitle, getString(R.string.main_activity_menu_clothes_list)));
+            this.setTitle(String.format(activityBaseTitle, getString(R.string.main_activity_menu_clothes_list)));
         }
         else if (itemId == R.id.settings) {
             loadFragment(MenuFragment.class);
-            activityTitle.setText(String.format(activityBaseTitle, getString(R.string.main_activity_menu_settings)));
+            this.setTitle(String.format(activityBaseTitle, getString(R.string.main_activity_menu_settings)));
         }
         else if (itemId == R.id.email) {
             loadFragment(EmailFragment.class);
-            activityTitle.setText(String.format(activityBaseTitle, getString(R.string.main_activity_menu_email)));
+            this.setTitle(String.format(activityBaseTitle, getString(R.string.main_activity_menu_email)));
         }
 
         return true;
