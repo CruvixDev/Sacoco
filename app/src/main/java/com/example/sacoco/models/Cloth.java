02d4333f9f55df5ea75a6f30985a292e4b17925c
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2024-01-14 22:16:46 +0100

    Complete the Cloth entity

--- app/src/main/java/com/example/sacoco/models/Cloth.java
+++ app/src/main/java/com/example/sacoco/models/Cloth.java
@@ -1,16 +1,17 @@
 package com.example.sacoco.models;
 
-import android.net.Uri;
+import java.net.URI;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Cloth {
     private UUID clothUUID;
     private String clothName;
     private ClothTypeEnum clothType;
-    private Uri imagePath;
+    private URI imagePath;
 
-    public Cloth(UUID clothUUID, String clothName, ClothTypeEnum clothType, Uri imagePath) {
+    public Cloth(UUID clothUUID, String clothName, ClothTypeEnum clothType, URI imagePath) {
         this.clothUUID = clothUUID;
         this.clothName = clothName;
         this.clothType = clothType;
@@ -33,7 +34,7 @@ public class Cloth {
         return clothType;
     }
 
-    public Uri getImagePath() {
+    public URI getImagePath() {
         return imagePath;
     }
 
@@ -45,7 +46,15 @@ public class Cloth {
         this.clothType = clothType;
     }
 
-    public void setImagePath(Uri imagePath) {
+    public void setImagePath(URI imagePath) {
         this.imagePath = imagePath;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cloth)) return false;
+        Cloth cloth = (Cloth) o;
+        return Objects.equals(clothUUID, cloth.clothUUID);
     }
 }
