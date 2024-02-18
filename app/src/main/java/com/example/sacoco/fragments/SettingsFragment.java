a321ae173b132153915d188fe5d734ec7e32b37b
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2024-02-18 12:39:49 +0100

    Get app version in SettingsFragment

--- app/src/main/java/com/example/sacoco/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/sacoco/fragments/SettingsFragment.java
@@ -9,12 +9,36 @@ import androidx.preference.PreferenceFragmentCompat;
 import com.example.sacoco.R;
 import com.example.sacoco.viewmodels.BagViewModel;
 
+import java.util.Objects;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class SettingsFragment extends PreferenceFragmentCompat {
+    private Disposable disposable;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
         Preference appVersionPreference = findPreference("preferencesAppVersion");
 
         BagViewModel bagViewModel = new ViewModelProvider(requireActivity()).get(BagViewModel.class);
+
+        disposable = bagViewModel.getAppVersion()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(appVersion ->
+                        Objects.requireNonNull(appVersionPreference).setSummary(appVersion));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        //Unsubscribe to avoid memory leak
+        if (disposable != null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
     }
 }
