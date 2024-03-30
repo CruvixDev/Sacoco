b40094c3b00c8abc0891527546ecdcdeb67c5f85
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2024-03-30 22:35:48 +0100

    Correct some warnings

--- app/src/main/java/com/example/sacoco/fragments/ClothesListFragment.java
+++ app/src/main/java/com/example/sacoco/fragments/ClothesListFragment.java
@@ -7,8 +7,6 @@ import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
