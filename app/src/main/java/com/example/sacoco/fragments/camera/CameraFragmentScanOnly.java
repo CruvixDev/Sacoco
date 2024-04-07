cfd7e96cdbed79de096647fb79a9e8a165ef88be
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2024-04-07 11:30:26 +0200

    Create the two missing use cases for camera usage

---
+++ app/src/main/java/com/example/sacoco/fragments/camera/CameraFragmentScanOnly.java
@@ -0,0 +1,13 @@
+package com.example.sacoco.fragments.camera;
+
+public class CameraFragmentScanOnly extends AbstractCameraFragment{
+    @Override
+    protected void setupCameraUseCases() {
+
+    }
+
+    @Override
+    protected void onCameraFragmentFinish() {
+
+    }
+}
---
+++ app/src/main/java/com/example/sacoco/fragments/camera/CameraFragmentTakeOnly.java
@@ -0,0 +1,13 @@
+package com.example.sacoco.fragments.camera;
+
+public class CameraFragmentTakeOnly extends AbstractCameraFragment{
+    @Override
+    protected void setupCameraUseCases() {
+
+    }
+
+    @Override
+    protected void onCameraFragmentFinish() {
+
+    }
+}
